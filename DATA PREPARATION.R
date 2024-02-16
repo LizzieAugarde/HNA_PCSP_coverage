d8774882b05eeff47f48d64e4111d6eef8329bd3
LizzieAugarde <lizzie994@googlemail.com>
2024-02-16 11:03:51 +0000

    Moving IMD add in to earlier in DATA PREPARATION script

--- DATA PREPARATION.R
+++ DATA PREPARATION.R
@@ -13,14 +13,15 @@ library(tidyverse)
 library(janitor)
 
 casref01 <- createConnection()
+cas2401 <- createConnection(port = 1525, sid = "cas2401")
 
-#pulling from SQL
-raw_data <- dbGetQueryOracle(casref01, "SELECT * FROM hna_pcsp_2021diags", rowlimit = NA)
+#pulling from SQL 
+raw_data <- dbGetQueryOracle(cas2401, "SELECT * FROM HNA_PCSP_2021DIAGS", rowlimit = NA)
 
 raw_data <- clean_names(raw_data)
 
-hna_pcsp_data <- raw_data %>% unique() 
-#need to redo all this NUMBERS DON'T MAKE SENSE
+hna_pcsp_data <- raw_data %>% unique() #488976 unique rows 
+
 hna_pcsp_data <- hna_pcsp_data %>%
   separate(event_property_1, c("point_of_pathway", "offered_code", "staff_role"), ":") %>%
   mutate(hna = case_when(event_type == 20 ~ "Y", TRUE ~ "N"),
@@ -28,39 +29,33 @@ hna_pcsp_data <- hna_pcsp_data %>%
 
 length(unique(hna_pcsp_data$patientid)) #311627 patients (329665 diagnoses in NS publication, remember DCO and C44)
 
-#splitting into HNA/PCSP/neither
-no_events <- hna_pcsp_data %>% filter(hna == "N" & pcsp == "N") %>% unique()
-hna_data <- hna_pcsp_data %>% filter(hna == "Y") %>% unique()
-pcsp_data <- hna_pcsp_data %>% filter(pcsp == "Y") %>% unique()
-
-write.csv(no_events, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/Patients with no events raw data RCRD 20240116.csv")
-write.csv(hna_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/HNA events raw data RCRD 20240116.csv")
-write.csv(pcsp_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/PCSP events raw data RCRD 20240116.csv")
 
 #adding IMD data 
 query <- "select a.patientid,
                  g.lsoa11_code,
                  d.imd19_decile_lsoas,
                  d.imd19_quintile_lsoas
-          from av2021.at_tumour_england@casref01 a 
+          from analysiselizabethaugarde.hna_pcsp_2021diags@cas2401 a 
           left join av2021.at_geography_england@casref01 g on a.tumourid = g.tumourid
-          left join imd.imd2019_equal_lsoas@casref01 d on g.lsoa11_code = d.lsoa11_code
-          where a.diagnosisyear = '2021'"#need this to select the most recent deprivation 
+          left join imd.imd2019_equal_lsoas@casref01 d on g.lsoa11_code = d.lsoa11_code"
 
 imd_data <- dbGetQueryOracle(casref01, query, rowlimit = NA)
 
 imd_data <- imd_data %>% select(-LSOA11_CODE) %>% unique() 
 
-hna_pcsp_data <- left_join(hna_pcsp_data, imd_data, by = c("patientid" = "PATIENTID"), relationship = "one-to-many")
+hna_pcsp_data <- left_join(hna_pcsp_data, imd_data, by = c("patientid" = "PATIENTID"), relationship = "many-to-one")
 
 
-#keeping earliest HNA/PCSP for each patient
-need to rank across the hnas/pcsps for each person to pick the record on each date with the most info
+#splitting into HNA/PCSP/neither
+no_events <- hna_pcsp_data %>% filter(hna == "N" & pcsp == "N") %>% unique() #216669 patients with no events
+hna_data <- hna_pcsp_data %>% filter(hna == "Y") %>% unique() #151216 HNA records
+pcsp_data <- hna_pcsp_data %>% filter(pcsp == "Y") %>% unique() #121091 PCSP records
 
-hna_data <- hna_data %>%
-  group_by(patientid) %>%
-  slice_min(order_by = as.Date(event_date))
+length(unique(no_events$patientid)) #check these patients are unique 
+length(unique(hna_data$patientid)) #85798 unique patients with an HNA record
+length(unique(pcsp_data$patientid)) #81216 unique patients with a PCSP record
 
-pcsp_data <- pcsp_data %>%
-  group_by(patientid) %>%
-  slice_min(order_by = as.Date(event_date))
+write.csv(no_events, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/Patients with no events raw data RCRD 20240216.csv")
+write.csv(hna_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/HNA events raw data RCRD 20240216.csv")
+write.csv(pcsp_data, "N:/INFO/_LIVE/NCIN/Macmillan_Partnership/HNAs/COSD level 3 analysis/Data/PCSP events raw data RCRD 20240216.csv")
+
