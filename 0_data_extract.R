960a81ade36beb86f6ca9e59f288e29813f7637d
Elizabeth Augarde <ELAU2@hscic.gov.uk>
2024-09-17 12:24:57 +0100

    Moved SQL extraction to new file

---
+++ 0_data_extract.R
@@ -0,0 +1,116 @@
+################ HNA/PCSP coverage analysis - data extraction ###########################
+
+#SQL extracts for patient cohort and linked HNA/PCSP records from RCRD
+
+#Created September 2024 by Lizzie Augarde 
+###################################################################################
+
+
+library(NDRSAfunctions)
+
+casref01 <- createConnection()
+cas2407 <- createConnection(port = 1525, sid = "cas2407")
+
+###### EXTRACTING PATIENT COHORT #######
+#patient cohort (all 2021 diagnoses exc C44, all ages, non DCO and not diagnosed same day as death), including demogs----
+query <- "select * from (
+  select pat.patientid,
+  tum.tumourid,
+  tum.diagnosisdatebest,
+  tum.site_icd10r4_o2_3char_from2013,
+  tum.stage_pi_detail,
+  tum.stage_best_system,
+  tum.stage_best,
+  tum.gender,
+  tum.age,
+  tum.ethnicity,
+  tum.deathdatebest,
+  tum.diag_trust,
+  imd.imd19_decile_lsoas,
+  site.ndrs_main,
+  rank () over (partition by pat.patientid order by tum.diagnosisdatebest, tum.tumourid asc) as rank
+  from av2021.at_patient_england@casref01 pat 
+  left join av2021.at_tumour_england@casref01 tum on pat.patientid = tum.patientid
+  left join analysispollyjeffrey.at_site_england@casref01 site on tum.tumourid = site.tumourid --added 25/07/2024
+  left join av2021.at_geography_england@casref01 geo on tum.tumourid = geo.tumourid
+  left join imd.imd2019_equal_lsoas@casref01 imd on geo.lsoa11_code = imd.lsoa11_code
+  where tum.diagnosisyear = 2021
+  and tum.cascade_inci_flag = 1 
+  and tum.dco = 'N'
+  and tum.dedup_flag = 1
+  --and tum.age >17 --removed 17/07/2024 (all ages due to lack of specificity in guidance)
+  and ((tum.deathdatebest is null) or (tum.diagnosisdatebest != tum.deathdatebest)) 
+  and ((tum.deathdatebest is null) or (tum.deathdatebest - tum.diagnosisdatebest > 0)) 
+  and substr(tum.site_icd10_o2_3char, 1, 1) = 'C' 
+  and tum.site_icd10_o2_3char <> 'C44'
+  and ((tum.gender = '1' and tum.site_icd10_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58') --added 16/09/2024 following QA, limiting to tumours matching gender
+        and tum.site_icdo3rev2011_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58'))
+      or 
+        (tum.gender = '2' and tum.site_icd10_3char not in ('C60','C61','C62','C63')
+        and tum.site_icdo3rev2011_3char not in ('C60','C61','C62','C63'))
+      or tum.site_icd10 is null or tum.site_icdo3rev2011_3char is null))
+      subquery
+where rank = 1"
+
+patient_cohort <- dbGetQueryOracle(casref01, query, rowlimit = NA)
+
+
+###### EXTRACTING PATIENT COHORT LINKED TO RCRD TO FIND THOSE MISSING FROM RCRD #######
+query <- "select * from (
+  select pat.patientid, 
+  rp.nhsnumber
+  from av2021.at_patient_england@casref01 pat 
+  left join av2021.at_tumour_england@casref01 tum on pat.patientid = tum.patientid
+  left join analysisncr.at_rapid_pathway@cas2407 rp on pat.nhsnumber = rp.nhsnumber
+  where tum.diagnosisyear = 2021
+  and tum.cascade_inci_flag = 1 
+  and tum.dco = 'N'
+  and tum.dedup_flag = 1
+  and ((tum.deathdatebest is null) or (tum.diagnosisdatebest != tum.deathdatebest)) 
+  and ((tum.deathdatebest is null) or (tum.deathdatebest - tum.diagnosisdatebest > 0)) 
+  and substr(tum.site_icd10_o2_3char, 1, 1) = 'C' 
+  and tum.site_icd10_o2_3char <> 'C44'
+  and ((tum.gender = '1' and tum.site_icd10_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58') --added 16/09/2024 following QA, limiting to tumours matching gender
+        and tum.site_icdo3rev2011_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58'))
+      or 
+        (tum.gender = '2' and tum.site_icd10_3char not in ('C60','C61','C62','C63')
+        and tum.site_icdo3rev2011_3char not in ('C60','C61','C62','C63'))
+      or tum.site_icd10 is null or tum.site_icdo3rev2011_3char is null))"
+
+rcrd_missing_diags <- dbGetQueryOracle(casref01, query, rowlimit = NA)
+
+
+###### EXTRACTING LINKED HNA AND PCSP RECORDS #######
+query <- "select * from (
+  select pat.patientid,
+  rp.nhsnumber as rp_nhsnumber,
+  rp.patientid as rp_patientid,       
+  tum.tumourid,
+  tum.diagnosisdatebest,
+  rp.event_type,
+  rp.event_property_1,
+  rp.event_date,
+  rp.trust_code,
+  rank () over (partition by pat.patientid order by tum.diagnosisdatebest, tum.tumourid asc) as rank
+  from av2021.at_patient_england@casref01 pat 
+  left join av2021.at_tumour_england@casref01 tum on pat.patientid = tum.patientid
+  left join analysisncr.at_rapid_pathway@cas2407 rp on pat.nhsnumber = rp.nhsnumber 
+  where tum.diagnosisyear = 2021
+  and tum.cascade_inci_flag = 1 
+  and tum.dco = 'N'
+  and tum.dedup_flag = 1
+  --and tum.age >17 ---removed 17/07/2024
+  and ((tum.deathdatebest is null) or (tum.diagnosisdatebest != tum.deathdatebest)) 
+  and ((tum.deathdatebest is null) or (tum.deathdatebest - tum.diagnosisdatebest > 0)) 
+  and substr(tum.site_icd10_o2_3char, 1, 1) = 'C' 
+  and tum.site_icd10_o2_3char <> 'C44'
+  and ((tum.gender = '1' and tum.site_icd10_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58') --added 16/09/2024 following QA, limiting to tumours matching gender
+        and tum.site_icdo3rev2011_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58'))
+      or 
+        (tum.gender = '2' and tum.site_icd10_3char not in ('C60','C61','C62','C63')
+        and tum.site_icdo3rev2011_3char not in ('C60','C61','C62','C63'))
+      or tum.site_icd10 is null or tum.site_icdo3rev2011_3char is null)
+  and rp.event_type in (20, 24))
+where rank = 1"
+
+raw_hna_pcsp_data <- dbGetQueryOracle(casref01, query, rowlimit = NA)
--- 1_data_preparation.R
+++ 1_data_preparation.R
@@ -40,6 +40,7 @@ query <- "select * from (
   left join analysispollyjeffrey.at_site_england@casref01 site on tum.tumourid = site.tumourid --added 25/07/2024
   left join av2021.at_geography_england@casref01 geo on tum.tumourid = geo.tumourid
   left join imd.imd2019_equal_lsoas@casref01 imd on geo.lsoa11_code = imd.lsoa11_code
+  left join analysisncr.at_rapid_pathway@cas2407 rp on pat.nhsnumber = rp.nhsnumber --added 16/09/2024, only patients with a diagnosis in RCRD
   where tum.diagnosisyear = 2021
   and tum.cascade_inci_flag = 1 
   and tum.dco = 'N'
@@ -48,10 +49,19 @@ query <- "select * from (
   and ((tum.deathdatebest is null) or (tum.diagnosisdatebest != tum.deathdatebest)) 
   and ((tum.deathdatebest is null) or (tum.deathdatebest - tum.diagnosisdatebest > 0)) 
   and substr(tum.site_icd10_o2_3char, 1, 1) = 'C' 
-  and tum.site_icd10_o2_3char <> 'C44') 
+  and tum.site_icd10_o2_3char <> 'C44'
+  and ((tum.gender = '1' and tum.site_icd10_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58') --added 16/09/2024 following QA, limiting to tumours matching gender
+        and tum.site_icdo3rev2011_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58'))
+      or 
+        (tum.gender = '2' and tum.site_icd10_3char not in ('C60','C61','C62','C63')
+        and tum.site_icdo3rev2011_3char not in ('C60','C61','C62','C63'))
+      or tum.site_icd10 is null or tum.site_icdo3rev2011_3char is null))
+      subquery
 where rank = 1"
 
-patient_cohort <- dbGetQueryOracle(cas2407, query, rowlimit = NA)
+REDO THIS WITH PATHWAY NHS NUMBER 
+
+patient_cohort <- dbGetQueryOracle(casref01, query, rowlimit = NA)
 
 patient_cohort <- patient_cohort |> 
   clean_names() |>
@@ -96,7 +106,7 @@ query <- "select * from (
   rank () over (partition by pat.patientid order by tum.diagnosisdatebest, tum.tumourid asc) as rank
   from av2021.at_patient_england@casref01 pat 
   left join av2021.at_tumour_england@casref01 tum on pat.patientid = tum.patientid
-  left join analysisncr.at_rapid_pathway@cas2406 rp on pat.nhsnumber = rp.nhsnumber 
+  left join analysisncr.at_rapid_pathway@cas2407 rp on pat.nhsnumber = rp.nhsnumber 
   where tum.diagnosisyear = 2021
   and tum.cascade_inci_flag = 1 
   and tum.dco = 'N'
@@ -106,10 +116,16 @@ query <- "select * from (
   and ((tum.deathdatebest is null) or (tum.deathdatebest - tum.diagnosisdatebest > 0)) 
   and substr(tum.site_icd10_o2_3char, 1, 1) = 'C' 
   and tum.site_icd10_o2_3char <> 'C44'
+  and ((tum.gender = '1' and tum.site_icd10_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58') --added 16/09/2024 following QA, limiting to tumours matching gender
+        and tum.site_icdo3rev2011_3char not in ('C51','C52','C53','C54','C55','C56','C57','C58'))
+      or 
+        (tum.gender = '2' and tum.site_icd10_3char not in ('C60','C61','C62','C63')
+        and tum.site_icdo3rev2011_3char not in ('C60','C61','C62','C63'))
+      or tum.site_icd10 is null or tum.site_icdo3rev2011_3char is null)
   and rp.event_type in (20, 24))
 where rank = 1"
 
-raw_hna_pcsp_data <- dbGetQueryOracle(cas2407, query, rowlimit = NA)
+raw_hna_pcsp_data <- dbGetQueryOracle(casref01, query, rowlimit = NA)
 
 
 ###### INITIAL CLEANING AND LINK HNA/PCSP DATA WITH COHORT #######
@@ -133,10 +149,11 @@ raw_hna_pcsp_data <- raw_hna_pcsp_data |>
 all_records <- sum(raw_hna_pcsp_data$hna == "Y" | raw_hna_pcsp_data$pcsp == "Y") 
 
 
-###### REMOVING DUPLICATES AND CALCULATING INITIAL NUMBERS #######
+###### REMOVING DUPLICATES, INVALID OFFERED CODES, AND CALCULATING INITIAL NUMBERS #######
 hna_pcsp_data <- raw_hna_pcsp_data |>
   unique() |> #removing duplicate rows
-  separate(event_property_1, c("point_of_pathway", "offered_code", "staff_role"), ":") 
+  separate(event_property_1, c("point_of_pathway", "offered_code", "staff_role"), ":") |>
+  mutate(offered_code = str_replace(offered_code, "00", "0")) #adjusting errors in offered codes
 
 denom_size <- length(unique(patient_cohort$patientid)) #total number of patients in the cohort 
 unique_hnas_pcsps <- sum(hna_pcsp_data$hna == "Y" | hna_pcsp_data$pcsp == "Y") #total number of unique HNA and PCSP records 
@@ -149,6 +166,12 @@ pcsps_notoff <- sum(hna_pcsp_data$pcsp == "Y" & hna_pcsp_data$offered_code == "0
 
 hna_pcsp_data <- hna_pcsp_data |>
   filter(offered_code != "04" | is.na(offered_code)) #counting NA or invalid offered_code as offered
+
+#removing records with invalid offered codes
+hna_pcsp_data <- hna_pcsp_data |>
+  filter(offered_code != "04") |> #excluding records which are "Not offered"
+  filter(offered_code != "") |> #removing weird codes
+  filter(nchar(offered_code) <3) |> #removing any remaining invalid offered codes
 
 unique_off_hnas_pcsps <- sum(hna_pcsp_data$hna == "Y" | hna_pcsp_data$pcsp == "Y") #total unique records of an OFFERED HNA/PCSP 
 unique_off_hnas <- sum(hna_pcsp_data$hna == "Y") #total unique OFFERED HNA records
@@ -208,10 +231,25 @@ both <- length(intersect(ids_hna, ids_pcsp)) #patients with both
 
 
 ###### RANKING HNAs/PCSPs PER PATIENT BY DATE ######
-rank_by_date <- function(data, pid_col, date_col) {
+#indicating completeness of rows based on point in pathway and staff role 
+#so if there are 2 HNAs/PCSPs on the same day, the more complete record is ranked first 
+hna_data <- hna_data |>
+  mutate(comp = case_when(point_of_pathway=="" & staff_role=="" ~ 0, 
+                          point_of_pathway=="" & staff_role!="" ~ 1,
+                          point_of_pathway!="" & staff_role=="" ~ 1,
+                          point_of_pathway!="" & staff_role!="" ~ 2))
+
+pcsp_data <- pcsp_data |>
+  mutate(comp = case_when(point_of_pathway=="" & staff_role=="" ~ 0, 
+                          point_of_pathway=="" & staff_role!="" ~ 1,
+                          point_of_pathway!="" & staff_role=="" ~ 1,
+                          point_of_pathway!="" & staff_role!="" ~ 2))
+
+
+rank_by_date <- function(data, pid_col, date_col, comp_col) {
   data |>
     group_by({{ pid_col }}) |>
-    arrange({{ date_col }}) |>
+    arrange({{ date_col }}, {{ comp_col }}) |>
     mutate(rank = row_number())
 }
 
@@ -283,5 +321,6 @@ patient_level_data <- patient_level_data |>
          pcsp_count = ifelse(is.na(pcsp_count), 0, pcsp_count)) |>
   select(-rank, hna_rank, pcsp_rank) |>
   mutate(hna_status = ifelse(hna_count > 0, "Has HNA", "No HNA"),
-         pcsp_status = ifelse(pcsp_count > 0, "Has PCSP", "No PCSP"))
+         pcsp_status = ifelse(pcsp_count > 0, "Has PCSP", "No PCSP")) |>
+  ungroup()
 
